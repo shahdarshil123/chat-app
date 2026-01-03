f487bf9cfaaa9e42b4423f80b0fa88da1d72a08c
shahdarshil123 <darshilshah622@gmail.com>
2026-01-03 16:02:38 -0500

    changes for Redis configuration for docker

--- server/src/redis/redis.js
+++ server/src/redis/redis.js
@@ -1,8 +1,11 @@
 // single Redis connection for entire app
 import Redis from "ioredis";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const redis = new Redis({
-  host: "127.0.0.1",
+  host: process.env.REDIS_HOST || "chat_redis",
   port: 6379,
 });
 
