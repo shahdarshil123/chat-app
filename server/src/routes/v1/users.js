59fec719ef9c46da5d4b144dcaf04e1790b7184b
shahdarshil123 <darshilshah622@gmail.com>
2026-01-16 22:14:52 -0500

    removed commented code

--- server/src/routes/v1/users.js
+++ server/src/routes/v1/users.js
@@ -40,51 +40,6 @@ router.get("/search", requireAuth, validate({ query: searchUserSchema }), async 
     }
 });
 
-// router.post("/create", requireAuth, validate(createUserSchema), async (req, res) => {
-//     try {
-//         const { username, email, password, displayName } = req.body;
-
-//         // Validation logic:
-//         if (!username) {
-//             console.log("Invalid Username")
-//             return res.status(400).json({ error: "Invalid Username" });
-//         }
-//         if (!email) {
-//             console.log("Invalid Email")
-//             return res.status(400).json({ error: "Invalid Email" });
-//         }
-//         if (!password) {
-//             console.log("Invalid Password")
-//             return res.status(400).json({ error: "Invalid Password" });
-//         }
-//         if (!displayName) {
-//             console.log("Invalid Display Name");
-//             return res.status(400).json({ error: "Invalid Display Name" });
-//         }
-
-//         const userByEmail = await getUserByEmailService(email);
-//         if(userByEmail){
-//             return res.status(403).json({error: `User with email:${email} already exists`});
-//         }
-
-//         const userByUsername = await getUserByUsernameService(username);
-//         if(userByUsername){
-//             return res.status(403).json({error: `User with username: ${username} already exists`});
-//         }
-
-//         const user = await createUserService(username, email, password, displayName);
-//         console.log(user);
-//         if (!user) {
-//             return res.status(500).json({ error: 'Failed to create the user' });
-//         }
-
-//         return res.json({ user });
-//     }
-//     catch (error) {
-//         console.error('Get user error:', error);
-//         res.status(500).json({ error: 'Failed to get the user' });
-//     }
-// });
 
 
 router.get('/:id', requireAuth, validate({params: userIdParamSchema}), async (req, res) => {
